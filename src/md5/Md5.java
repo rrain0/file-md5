80665e78e2e3aeb050e953d1b87c63a9eb1a04ee
rRain <rrainpath@gmail.com>
2022-01-30 22:49:22 +0800

    FileRead is ready

--- src/md5/Md5.java
+++ src/md5/Md5.java
@@ -1,34 +1,80 @@
 package md5;
 
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 public class Md5 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Md5().run();
     }
 
 
 
-    public final Set<SourceInfo> paths = Set.of(
+    public final Set<SourceInfo> sources = Set.of(
         new SourceInfo("D:\\ТОРРЕНТЫ", "D:"),
         new SourceInfo("E:\\[test]", "E: 2.5\"")
     );
 
-    private final Map<Object, List<SourceInfo>> sourceMap = paths.stream()
-        .collect(Collectors.groupingBy(SourceInfo::threadId));
+    private static class SourceInfoBox {
+        int curr = 0;
+        final List<SourceInfo> sourceInfoList;
 
-    private final Set<SourceInfo> paused = new HashSet<>(paths);
+        public SourceInfoBox(List<SourceInfo> sourceInfoList) {
+            this.sourceInfoList = sourceInfoList;
+        }
 
-    public void run(){
-
+        int len(){ return sourceInfoList.size(); }
+        SourceInfo get(int id){ return sourceInfoList.get(id); }
     }
 
-    public boolean canWork(SourceInfo sourceInfo){
-        return !paused.contains(sourceInfo);
+    private final Map<Object, SourceInfoBox> sourceMap = sources.stream()
+        .collect(Collectors.groupingBy(SourceInfo::threadId, Collectors.collectingAndThen(Collectors.toList(), SourceInfoBox::new)));
+
+    private final Set<SourceInfo> paused = new HashSet<>(sources);
+
+    //private final Object sync = new Object();
+
+    public void run() throws InterruptedException {
+        start();
+        work();
     }
 
+    synchronized private void start(){
+        sourceMap.forEach((id,sib)->{
+            var source = sib.get(0);
+            new Thread(new FileReadTask(sib.get(0), , this)).start();
+            paused.remove(source);
+        });
+    }
+
+    synchronized private void work() throws InterruptedException {
+        while (!sourceMap.isEmpty()){
+            sourceMap.forEach((id,sib)->{
+                if (paused.containsAll(sib.sourceInfoList)) {
+                    sib.curr = (sib.curr+1) % sib.len();
+                    paused.remove(sib.sourceInfoList.get(sib.curr));
+                }
+            });
+            this.notifyAll();
+            this.wait();
+        }
+    }
+
+    synchronized public void awaitForWork(SourceInfo sourceInfo) throws InterruptedException {
+        while (paused.contains(sourceInfo)) this.wait();
+    }
+
+    synchronized public void oneFileWasRead(SourceInfo sourceInfo){
+        paused.add(sourceInfo);
+        this.notifyAll();
+    }
+
+    synchronized public void workFinished(SourceInfo sourceInfo){
+        paused.remove(sourceInfo);
+        var sib = sourceMap.get(sourceInfo.threadId());
+        sib.sourceInfoList.remove(sourceInfo);
+        if (sib.len()==0) sourceMap.remove(sourceInfo.threadId());
+        notifyAll();
+    }
 }
