06a42a63d7eedd18d53ed35aa0145c65eaa138a9
rRain <rrainpath@gmail.com>
2022-01-30 11:57:18 +0800

    Implementing new architecture 2

--- src/md5/Md5.java
+++ src/md5/Md5.java
@@ -1,6 +1,9 @@
 package md5;
 
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 
 public class Md5 {
 
@@ -15,9 +18,17 @@ public class Md5 {
         new SourceInfo("E:\\[test]", "E: 2.5\"")
     );
 
+    private final Map<Object, List<SourceInfo>> sourceMap = paths.stream()
+        .collect(Collectors.groupingBy(SourceInfo::threadId));
+
+    private final Set<SourceInfo> paused = new HashSet<>(paths);
+
     public void run(){
 
     }
 
+    public boolean canWork(SourceInfo sourceInfo){
+        return !paused.contains(sourceInfo);
+    }
 
 }
