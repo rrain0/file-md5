78bb1d5143bf77491b8fe6407ca8f6665439b144
rRain <rrainpath@gmail.com>
2022-03-15 13:22:41 +0800

    little changes

--- src/main/java/md5/Md5.java
+++ src/main/java/md5/Md5.java
@@ -24,6 +24,10 @@ import java.util.List;
 
 // todo надо сделать 1 диск - 1 одновременный поток, но перед этим проверить на ссд как он читает, есть ли разница, если 2 потока или 1
 
+// todo в одном источнике несколько путей
+
+// todo отображать source list в правильном порядке
+
 public class Md5 {
 
     public static void main(String[] args) {
@@ -45,13 +49,42 @@ public class Md5 {
             Source.builder().path("F:\\test\\3").tag(3).readThreadId("F").build()
         );*/
 
-        final List<Source> sources = List.of(
+        /*final List<Source> sources = List.of(
             Source.builder().path("K:\\GAMES\\GAMES 2\\Streets of Rage\\Street Of Rage Collection").
                 tag("Seagate").readThreadId("K").build(),
             Source.builder().path("G:\\[удалить]\\Street Of Rage Collection").
                 tag("DATA_TWO").readThreadId("G").build(),
             Source.builder().path("H:\\[удалить]\\Street Of Rage Collection").
                 tag("Ts64").readThreadId("H").build()
+        );*/
+
+        /*final List<Source> sources = List.of(
+            Source.builder().path("H:\\").
+                tag("H").readThreadId("H").build(),
+            Source.builder().path("E:\\ТОРРЕНТЫ").
+                tag("G").readThreadId("G").build()
+        );*/
+
+        /*final List<Source> sources = List.of(
+            Source.builder().path("L:\\d\\[ ] DISK D\\ЗАГРУЗКИ\\скобочные последовательности - Задача - E - Codeforces.html").
+                tag(1).readThreadId(1).build(),
+            Source.builder().path("F:\\скобочные последовательности - Задача - E - Codeforces.html").
+                tag(2).readThreadId(2).build()
+        );*/
+
+        // todo THERE IS BUG
+        /*final List<Source> sources = List.of(
+            Source.builder().path("H:\\SQL ДОРОФЕЕВ\\материалы\\DataBase 1 ноут").
+                tag(1).readThreadId(1).build(),
+            Source.builder().path("H:\\SQL ДОРОФЕЕВ\\материалы\\DataBase 2 телефон").
+                tag(2).readThreadId(1).build()
+        );*/
+
+        final List<Source> sources = List.of(
+            Source.builder().path("L:\\БЭКАПЫ\\D\\УЧЕБНИКИ СМ\\Начерт и Инженерка\\Начертательная геометрия\\Начерт олимпиада задания на подготовку").
+                tag(1).readThreadId(1).build(),
+            Source.builder().path("I:\\Начерт олимпиада задания на подготовку").
+                tag(2).readThreadId(2).build()
         );
 
 
